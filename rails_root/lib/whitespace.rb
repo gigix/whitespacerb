63e56baa16728bb17e05bc259d90438b88b6cb46
gigix1980 <gigix1980@f0385822-1148-11de-b0d4-7fec019aa467>
2009-03-15 12:27:54 +0000

    change algorithm

--- rails_root/lib/whitespace.rb
+++ rails_root/lib/whitespace.rb
@@ -5,21 +5,30 @@ class Whitespace
   TAB = "\t"
   NEWLINE = "\r\n"
   
+  WHITESPACES = SPACE + TAB + NEWLINE
+  
   def self.encode(str)
     base64 = Base64.encode64 str
     whitespaces = ''
     base64.length.times do |i|
-      whitespaces << (SPACE * base64[i])
+      whitespaces << SPACE * (base64[i] / 100) << TAB << SPACE * (base64[i] % 100 / 10) << TAB << SPACE * (base64[i] % 10)
       whitespaces << NEWLINE
     end
-    whitespaces
+    "COPY_ME_WHOLE{{#{whitespaces}}}END_COPY"
   end
   
-  def self.decode(whitespaces)
+  def self.decode(str)
+    whitespaces = ''
+    str.length.times do |i|
+      whitespaces << str[i] if ('' << str[i]).strip.empty?
+    end
+    
     white_chars = whitespaces.split NEWLINE
     base64 = ''
     white_chars.each do |white|
-      base64 << white.length
+      hundred, ten, single = white.split TAB
+      single ||= ''
+      base64 << hundred.length * 100 + ten.length * 10 + single.length
     end
     Base64.decode64 base64
   end
